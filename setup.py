b9a5956954a188fd3c9a4cf6fcebd2ff0cb0ca47
Fmat34 <ferranmabe@gmail.com>
2023-10-01 01:15:21 +0200

    update long_description

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='py_nessus_pro',
-    version='0.6',
+    version='0.8',
     packages=find_packages(),
     install_requires=[
         'selenium',
@@ -13,7 +13,19 @@ setup(
     description='Python library for managing Nessus Professional.',
     url='https://github.com/Matbe34/py-nessus-pro',
     classifiers=['Programming Language :: Python :: 3'],
-    long_description="""A Python module that provides a high-level interface for interacting with a Nessus vulnerability scanner. It provides Nessus Professional with an interface to expand the read-omly API and be able to launch scans and modify them.
+    long_description="""PyNessusPro
+===========
+    
+``py_nessus_pro`` is a Python module that provides a high-level interface for interacting with a Nessus vulnerability scanner. The module uses the Nessus REST API to perform various operations, such as creating and managing scans, retrieving scan metadata and reports, and searching for scans by name or date. It provides Nessus Professional with an interface to expand the read-omly API and be able to launch scans and modify them.
+
+Installation
+------------
+
+To install ``py_nessus_pro``\, simply run:
+
+.. code:: 
+
+   pip install py-nessus-pro
 
 Usage
 ------------
