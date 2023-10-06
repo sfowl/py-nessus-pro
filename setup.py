48c3c91ff2b155bfb1ee64b37eca555977f60529
Ferran <ferran.mateu@bmm.com>
2023-10-06 13:47:28 +0200

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='py_nessus_pro',
-    version='0.8.6',
+    version='0.9.0',
     packages=find_packages(),
     install_requires=[
          'selenium',
