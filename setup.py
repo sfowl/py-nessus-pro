f34f67b57a9bdd2b0dc02615bdd457812ade5329
Matbe34 <ferranmabe@gmail.com>
2023-11-03 15:06:58 +0100

    little fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='py_nessus_pro',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(),
     install_requires=[
          'selenium',
@@ -15,7 +15,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'py-nessus-pro = cli:app',
+            'py-nessus-pro=cli:app',
         ],
     },
     author='Matbe34',
