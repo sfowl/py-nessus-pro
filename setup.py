f716363fa72665f5fa65e5ce4428ae518b21c3c2
Matbe34 <ferranmabe@gmail.com>
2023-11-03 15:12:05 +0100

    rename cli

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='py_nessus_pro',
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(),
     install_requires=[
          'selenium',
@@ -15,7 +15,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'py-nessus-pro=cli:app',
+            'py-nessus-pro=py_nessus_pro_cli:app',
         ],
     },
     author='Matbe34',
